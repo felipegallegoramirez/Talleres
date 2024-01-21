3c0800342c37c4eed57c9df471ec4e4f3a5f2480
Felipe Gallego <felipegallego600@gmail.com>
2024-01-21 17:08:50 -0500

    first commit

---
+++ src/Frontend/src/app/app.module.ts
@@ -0,0 +1,42 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+import { Routes, RouterModule } from '@angular/router';
+
+import { AppComponent } from './app.component';
+
+
+
+import { HomeComponent } from './components/home/home.component';
+import { LoginComponent } from './components/login/login.component';
+import { AdmonComponent } from './components/admon/admon.component';
+import { ListCarsComponent } from './components/list-cars/list-cars.component';
+import { ViewCarsComponent } from './components/view-cars/view-cars.component';
+
+
+
+const routes: Routes = [
+  { path: 'login', component: LoginComponent },
+  { path: 'home', component: HomeComponent},
+  { path: 'admon', component: AdmonComponent},
+  { path: 'panel', component: ListCarsComponent },
+  { path: 'car/:id', component:ViewCarsComponent},
+];
+
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    HomeComponent,
+    LoginComponent,
+    AdmonComponent,
+    ListCarsComponent,
+    ViewCarsComponent
+  ],
+  imports: [
+    BrowserModule,
+    RouterModule.forRoot(routes, {useHash:true}),
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
