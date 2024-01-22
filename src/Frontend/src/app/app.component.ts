bb67993551f73ef29e1841a978413d57ff92819c
Felipe Gallego <felipegallego600@gmail.com>
2024-01-22 16:41:54 -0500

    car-list

--- src/Frontend/src/app/app.component.ts
+++ src/Frontend/src/app/app.component.ts
@@ -6,9 +6,7 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'Frontend';
-
-  
+  title = 'Taller';
 
   ocultar(){
     const nav=document.querySelector("#nav")
