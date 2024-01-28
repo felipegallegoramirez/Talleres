635c0bffd651fb678b6fa319cd402d769364c2fd
Felipe Gallego <felipegallego600@gmail.com>
2024-01-28 16:10:14 -0500

    admon

--- src/Frontend/src/app/components/admon/admon.component.ts
+++ src/Frontend/src/app/components/admon/admon.component.ts
@@ -3,13 +3,42 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-admon',
   templateUrl: './admon.component.html',
-  styleUrls: ['./admon.component.css']
+  styleUrls: ['./admon.component.css'],
 })
 export class AdmonComponent implements OnInit {
-
-  constructor() { }
-
+  constructor() {}
+  count:Array<any> =[]
   ngOnInit(): void {
   }
+
+
+
+
+  mostrar(name: Array<string>,ocultos:Array<string>) {
+    name.forEach(function (value) {
+      let x = document.getElementById(value);
+      if (x?.classList.contains('oculto')) {
+        x.classList.remove('oculto');
+      }
+    });
+    this.ocultar(ocultos)
+  }
+
+  ocultar(name: Array<string>) {
+    name.forEach(function (value) {
+      let x = document.getElementById(value);
+      if (!x?.classList.contains('oculto') && x) {
+        x.classList.add('oculto');
+      }
+    });
+  }
+
+  timeadd(){
+    this.count.push("1")
+  }
+  timeremove(){
+    this.count.pop()
+  }
+
 
 }
